2175c98a84aeb0983fe38812592f2c71ed30804b
vkhydras <victorkimaru8@gmail.com>
2024-03-29 15:54:29 +0300

    back

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
-// Routes
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 router.get('/profile', AuthController.profile);
