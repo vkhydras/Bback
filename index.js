fa575e19b2d3a4cd14e5e963370850ec584975f3
vkhydras <victorkimaru8@gmail.com>
2024-03-29 15:53:53 +0300

    back

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express();
 const routes = require('./routes/routes');
 const connectDB = require('./config/db');
 
-// Middleware
 app.use(express.json());
 
 connectDB();
 
-// Routes
 app.use('/api', routes);
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
