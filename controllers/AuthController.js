ff589da4c2d92fe9ed34e71b9534d1f64b6ea68f
vkhydras <victorkimaru8@gmail.com>
2024-03-29 15:51:34 +0300

    back

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -20,7 +20,6 @@ const AuthController = {
         return res.status(401).json({ message: 'Invalid email or password' });
       }
 
-      // Here you might generate a token and return it as response
       res.status(200).json({ message: 'Login successful', user });
     } catch (error) {
       res.status(500).json({ message: 'Failed to login', error: error.message });
@@ -28,7 +27,6 @@ const AuthController = {
   },
 
   async profile(req, res) {
-    // Assuming you have middleware to verify JWT token and attach user info to req.user
     const user = req.user;
     res.status(200).json({ user });
   }
